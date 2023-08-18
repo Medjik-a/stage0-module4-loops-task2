5ea312be2127953cf31527c06cf14ce061b8d0ab
medjik <med.akhmetov@gmail.com>
2023-08-18 14:30:48 +0600

    first attempt

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -1,6 +1,27 @@
 package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
+    public static boolean isPrime(int number){
+        if (number<2){
+            return false;
+        }
+        int i=2;
+        while(i<number){
+            if(number%i==0){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
     public void printPrimeNumbers(int printToInclusive) {
+        int i=0;
+        while (i<=printToInclusive){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+            i++;
+        }
+
     }
 }
